ed13f423bade454a068a476bb7ae9a70d8566baf
acx593 <dominik.martin@haw-hamburg.de>
2021-08-10 19:32:53 +0200

    Bot mit dem Server verbunden

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import discord
+from discord.ext import commands
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# client erstellen für die Discord connection
+client = discord.ext.commands.Bot(command_prefix="$")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+# Das ist damit der Bot startet, wenn das ausgeführt wird
+client.run("ODc0NzA1Mzg4MzA0NDg2NTAy.YRK23w.KUKkT2dQs8hqrgaFSpHJOqQDjLg")
