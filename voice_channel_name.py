f65cec9d6acf7996d46f7b2f56d580e0ec524552
acx593 <dominik.martin@haw-hamburg.de>
2021-08-19 20:03:21 +0200

    Create Voice channel verbessert & Namens array für voice channel hinzugefügt

--- voice_channel_name.py
+++ voice_channel_name.py
@@ -1,3 +1,3 @@
 # Hier kommt die methode mit dem zufalls Array rein um die voice channels zufällige Namen zu geben
 
-names = ["hallo", "test1", "test2", "test3"]
+names = ["hallo", "test1", "Dominiks Voice channel", "Jakobs Voices Channel"]
